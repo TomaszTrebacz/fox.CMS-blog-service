4c72fb38a0917e1d993980f407aa5fd992317ceb
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-11 20:07:00 +0100

    refactor: update ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -4,6 +4,7 @@ module.exports = {
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['dist/database/entities/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
   cli: {
