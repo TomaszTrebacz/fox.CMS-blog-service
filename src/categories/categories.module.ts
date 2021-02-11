fad14b844fa88e5a6ce72a8ab929cd25b71ad242
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-11 20:16:09 +0100

    perf: change approach to importing resolvers

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,22 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesService } from './service/categories.service';
 import { CategoryEntity } from '../database/entities/category.entity';
 import { categoryExist, categoryUnique } from '../validators';
-import {
-  CreateCategoryResolver,
-  DeleteCategoryResolver,
-  EditCategoryResolver,
-  FindAllCategoriesResolver,
-  FindOneResolver,
-} from './resolvers';
+import * as Resolvers from './resolvers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoryEntity])],
   providers: [
-    CreateCategoryResolver,
-    DeleteCategoryResolver,
-    EditCategoryResolver,
-    FindAllCategoriesResolver,
-    FindOneResolver,
+    ...Object.values(Resolvers),
     CategoriesService,
     categoryExist,
     categoryUnique,
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,32 +3,10 @@ import { PostsService } from './service/posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from '../database/entities/post.entity';
 import { postExist } from '../validators';
-import {
-  ChangeCategoryPostResolver,
-  CreatePostResolver,
-  DeletePostResolver,
-  deleteUserPostResolver,
-  EditPostResolver,
-  FindAllResolver,
-  FindOneResolver,
-  FindUserPostsResolver,
-  PostsResolver,
-} from './resolvers';
+import * as Resolvers from './resolvers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostEntity])],
-  providers: [
-    PostsResolver,
-    FindOneResolver,
-    FindAllResolver,
-    FindUserPostsResolver,
-    EditPostResolver,
-    CreatePostResolver,
-    DeletePostResolver,
-    ChangeCategoryPostResolver,
-    deleteUserPostResolver,
-    PostsService,
-    postExist,
-  ],
+  providers: [...Object.values(Resolvers), PostsService, postExist],
 })
 export class PostsModule {}
